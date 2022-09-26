ae158de9d1a861fc58ba324cfc1321bf5d93efdf
ycyogesh <ycyogesh183@gmail.com>
2022-09-26 18:49:37 +0530

    added for name ----> id

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,43 +9,43 @@ import { FormsService } from '../forms.service';
 })
 export class FormComponent implements OnInit {
 
-  userName : string = ""
-  userMail : string = ""
-  usersList : Array<any> = [];
-  isUpdate : boolean = false;
-  userId : number = 0;
-  data : any
+  userName: string = ""
+  userMail: string = ""
+  usersList: Array<any> = [];
+  isUpdate: boolean = false;
+  userId: number = 0;
+  data: any
 
-  constructor(private appService : FormsService, private aRoute : ActivatedRoute, private router : Router) { }
+  constructor(private appService: FormsService, private aRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.aRoute.params.subscribe((data)=>{
+    this.aRoute.params.subscribe((data) => {
       console.log(data);
-      data.hasOwnProperty('id')?this.isUpdate=true:this.isUpdate=false;
+      data.hasOwnProperty('id') ? this.isUpdate = true : this.isUpdate = false;
 
-      if(this.isUpdate){
+      if (this.isUpdate) {
         this.userId = data['id']
         this.getUserById(this.userId)
       }
     })
   }
-  addUser(){
-    this.appService.addUser({name : this.userName,mail : this.userMail}).subscribe((data)=>{
+  addUser() {
+    this.appService.addUser({ name: this.userName, mail: this.userMail }).subscribe((data) => {
       console.log(data);
       this.router.navigate(['user-list']);
     })
   }
-getUserById(id : number){
-  this.appService.getUserById(id).subscribe((data)=>{
-    console.log("----------->",data);
-    this.data = data
-    this.userName = this.data.name
-    this.userMail = this.data.mail
-})
-}
+  getUserById(id: number) {
+    this.appService.getUserById(id).subscribe((data) => {
+      console.log("----------->", data);
+      this.data = data
+      this.userName = this.data.name
+      this.userMail = this.data.mail
+    })
+  }
 
-  updateUser(){
-    this.appService.editUser(this.userId,{'name' : this.userName, 'mail' : this.userMail}).subscribe((data)=>{
+  updateUser() {
+    this.appService.editUser(this.userId, { 'name': this.userName, 'mail': this.userMail }).subscribe((data) => {
       console.log(data);
       this.router.navigate(['user-list']);
     })
